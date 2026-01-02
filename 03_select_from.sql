ebe221aa56ddd3d2b1daf1755f312b76e986c98b
Thiago Thadeu <thiagothadeu11@gmail.com>
2026-01-02 19:21:33 -0300

    Até aula 8

---
+++ 03_select_from.sql
@@ -0,0 +1,15 @@
+SELECT IdProduto,
+       DescNomeProduto 
+
+/*
+    isso é um comentário 
+    de
+    múltiplas 
+    linhas
+*/
+
+FROM produtos
+-- isso não é executado
+
+-- outro comentário ignorado
+LIMIT 3
---
+++ bora_praticar/03_where.sql
@@ -0,0 +1,9 @@
+-- Selecione todos clientes com mais de 500 pontos
+
+SELECT idCliente,
+       qtdePontos
+
+FROM clientes
+
+WHERE qtdePontos > 500
+
