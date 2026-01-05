41fd69edefe6d395b202d6f115dab23a9c207963
Thiago Thadeu <thiagothadeu11@gmail.com>
2026-01-05 15:01:56 -0300

    Aulas 11 e 12 + exercício 9

--- exercicios/09_where.sql
+++ exercicios/09_where.sql
@@ -1,7 +1,15 @@
 -- Listar todas as transações adicionando uma coluna nova sinalizando 
 -- “alto”, “médio” e “baixo” para o valor dos pontos [<10 ; <500; >=500]
 
-SELECT IdTransacao,
-       QtdePontos
+SELECT *,
+       CASE
+            WHEN vlProduto < 10 THEN 'Baixo'
+            WHEN vlProduto < 500 THEN 'Medio'
+            ELSE 'Alto'
+       END AS 'ValorProduto'
 
-FROM transacoes
+FROM transacao_produto
+
+ORDER BY vlProduto
+
+
